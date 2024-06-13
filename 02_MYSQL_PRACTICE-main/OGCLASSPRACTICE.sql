e66121253ae20ba8784414d4ba4046604f976497
LKS9616 <lks609591@gmail.com>
2024-06-13 10:09:33 +0900

    commit OGCLASS

--- 02_MYSQL_PRACTICE-main/OGCLASSPRACTICE.sql
+++ 02_MYSQL_PRACTICE-main/OGCLASSPRACTICE.sql
@@ -13,6 +13,27 @@ create table if not exists class
     foreign key (subject_no) references favorite_subject (subject_no)
 );
 
+insert into class
+(student_no, student_name, gender, github_id, email, mbti, subject_no)
+values(null, '박태근', '남', ''),
+      (null, '지동현', '남', ''),
+      (null, '김강현', '남', ''),
+      (null, '이득규', '남', ''),
+      (null, '홍주현', '여', ''),
+      (null, '양혜연', '여', ''),
+      (null, '이규섭', '남', ''),
+      (null, '전유안', '남', ''),
+      (null, '박성은', '남', ''),
+      (null, '박효찬', '남', ''),
+      (null, '이의정', '남', ''),
+      (null, '권은혜', '여', ''),
+      (null, '권보현', '여', ''),
+      (null, '배하은', '여', ''),
+      (null, '강연진', '여', ''),
+      (null, '강진영', '여', '');
+
+
+
 drop table if exists favorite_subject;
 
 create table if not exists favorite_subject
